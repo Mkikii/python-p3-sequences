5855a43e5d2f5c1f0d6839c662dfe88339c6111f
Mkikii <maureenwkarimi@gmail.com>
2025-08-15 10:32:31 +0300

    all test passing

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    sequence = [0, 1]
+    while len(sequence) < length:
+        sequence.append(sequence[-1] + sequence[-2])
+
+    print(sequence)
+
+
+
